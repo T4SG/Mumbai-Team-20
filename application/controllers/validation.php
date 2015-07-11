c6ff27505dceed39456fde70c31dcc5ab4f22743
Nilesh Thadani <thadaninilesh@gmail.com>
2015-07-11 16:47:43 +0530

    validation update1

---
+++ application/controllers/validation.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: thadaninilesh
+ * Date: 11/7/15
+ * Time: 3:54 PM
+ */
+
+class validation extends CI_Controller{
+
+	public function index(){
+		$this->load->view('template/header');
+	}
+
+} 
---
+++ application/views/template/header.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thadaninilesh
+ * Date: 11/7/15
+ * Time: 3:37 PM
+ */
+?>
+<html>
+<head>
+	<link href="<?php echo CSS . "bootstrap.min.css"; ?>" rel="stylesheet">
+	<link href="<?php echo CSS . "animate.min.css"; ?>" rel="stylesheet">
+	<link href="<?php echo CSS . "custom.css"; ?>" rel="stylesheet">
+	<link href="<?php echo ASSET . "fonts/css/font-awesome.min.css"; ?>" rel="stylesheet">
+	<link href="<?php echo CSS . "icheck/flat/green.css"; ?>" rel="stylesheet">
+	<script src="<?php echo JS . "jquery.min.js"; ?>"></script>
+	<script src="<?php echo JS . "bootstrap.min.js"; ?>"></script>
+	<script src="<?php echo JS . "custom.js"; ?>"></script>
+</head>
+<body class="nav-md">
+
+<div class="container body">
+
+
+<div class="main_container">
+
+<div class="col-md-3 left_col">
+	<div class="left_col scroll-view">
+
+		<div class="navbar nav_title" style="border: 0;">
+			<a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>HHF</span></a>
+		</div>
+		<div class="clearfix"></div>
+
+		<!-- menu prile quick info -->
+		<div class="profile">
+			<div class="profile_pic">
+				<img src="images/img.jpg" alt="..." class="img-circle profile_img">
+			</div>
+			<div class="profile_info">
+				<span>Welcome,</span>
+				<h2>Happy Hearts Fund</h2>
+			</div>
+		</div>
+		<!-- /menu prile quick info -->
+
+		<br />
+
+		<!-- sidebar menu -->
+		<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
+			<div class="menu_section">
+				<h3>General</h3>
+				<ul class="nav side-menu">
+					<li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu" style="display: none">
+							<li><a href="index.html">Dashboard</a>
+							</li>
+							<li><a href="index2.html">Dashboard2</a>
+							</li>
+							<li><a href="index3.html">Dashboard3</a>
+							</li>
+						</ul>
+					</li>
+					<li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu" style="display: none">
+							<li><a href="form.html">General Form</a>
+							</li>
+							<li><a href="form_advanced.html">Advanced Components</a>
+							</li>
+							<li><a href="form_validation.html">Form Validation</a>
+							</li>
+							<li><a href="form_wizards.html">Form Wizard</a>
+							</li>
+							<li><a href="form_upload.html">Form Upload</a>
+							</li>
+							<li><a href="form_buttons.html">Form Buttons</a>
+							</li>
+						</ul>
+					</li>
+					<li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu" style="display: none">
+							<li><a href="general_elements.html">General Elements</a>
+							</li>
+							<li><a href="media_gallery.html">Media Gallery</a>
+							</li>
+							<li><a href="typography.html">Typography</a>
+							</li>
+							<li><a href="icons.html">Icons</a>
+							</li>
+							<li><a href="glyphicons.html">Glyphicons</a>
+							</li>
+							<li><a href="widgets.html">Widgets</a>
+							</li>
+							<li><a href="invoice.html">Invoice</a>
+							</li>
+							<li><a href="inbox.html">Inbox</a>
+							</li>
+							<li><a href="calender.html">Calender</a>
+							</li>
+						</ul>
+					</li>
+					<li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu" style="display: none">
+							<li><a href="tables.html">Tables</a>
+							</li>
+							<li><a href="tables_dynamic.html">Table Dynamic</a>
+							</li>
+						</ul>
+					</li>
+					<li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu" style="display: none">
+							<li><a href="chartjs.html">Chart JS</a>
+							</li>
+							<li><a href="chartjs2.html">Chart JS2</a>
+							</li>
+							<li><a href="morisjs.html">Moris JS</a>
+							</li>
+							<li><a href="echarts.html">ECharts </a>
+							</li>
+							<li><a href="other_charts.html">Other Charts </a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+		<!-- /sidebar menu -->
+
+
+	</div>
+</div>
+
+<!-- top navigation -->
+<div class="top_nav">
+
+	<div class="nav_menu">
+		<nav class="" role="navigation">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="">
+					<h1>Happy Hearts Fund</h1>
+				</li>
+
+				<li role="presentation" class="dropdown">
+
+					</ul>
+				</li>
+
+			</ul>
+		</nav>
+	</div>
+
+</div>
+<!-- /top navigation -->
